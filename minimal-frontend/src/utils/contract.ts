32c6a901a63cfeeaa2e99272b1c03ba7fd22afb2
chiranjeev13 <chiranjeevmishra13@gmail.com>
2025-05-23 22:16:02 +0530

    fix: update import path for PrivateWealth ABI in contract utility

--- minimal-frontend/src/utils/contract.ts
+++ minimal-frontend/src/utils/contract.ts
@@ -1,4 +1,4 @@
-import PrivateWealthabi from "../../../artifacts/PrivateWealth.sol/PrivateWealth.json" assert { type: "json" };
+import PrivateWealthabi from "../utils/abi/PrivateWealth.json" assert { type: "json" };
 import Address from "../../../broadcast/Deploy.s.sol/84532/run-latest.json" assert { type: "json" };
 
 export const PRIVATE_WEALTH_CONTRACT_ADDRESS =
